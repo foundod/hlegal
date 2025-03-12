1de7a89a5443767cf2c524cbc06bc49726f35b6b
margo cernysh <margocernys@gmail.com>
2025-03-12 16:32:29 +0200

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,55 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "../scss/style.scss";
 
-import '../scss/style.scss'
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
 
 (function () {
-    const isOpen = document.querySelector(".header__burger");
-    const isClose = document.querySelector(".header__menu-close");
-    const navigation = document.querySelector(".header__menu");
+  const isOpen = document.querySelector(".header__burger");
+  const isClose = document.querySelector(".header__menu-close");
+  const navigation = document.querySelector(".header__menu");
 
-    if (isOpen && navigation) {
-        isOpen.addEventListener("click", () => {
-            navigation.classList.toggle("active");
-        });
-    }
-
-    if (isClose && navigation) {
-        isClose.addEventListener("click", () => {
-            navigation.classList.remove("active");
-        });
-    }
-
-    document.addEventListener("click", (event) => {
-        if (navigation && !navigation.contains(event.target) && isOpen && !isOpen.contains(event.target)) {
-            navigation.classList.remove("active");
-        }
+  if (isOpen && navigation) {
+    isOpen.addEventListener("click", () => {
+      navigation.classList.toggle("active");
     });
+  }
+
+  if (isClose && navigation) {
+    isClose.addEventListener("click", () => {
+      navigation.classList.remove("active");
+    });
+  }
+
+  document.addEventListener("click", (event) => {
+    if (
+      navigation &&
+      !navigation.contains(event.target) &&
+      isOpen &&
+      !isOpen.contains(event.target)
+    ) {
+      navigation.classList.remove("active");
+    }
+  });
 })();
 
 (function () {
   // Отримуємо всі елементи вкладок
   const tabs = document.querySelectorAll(".tabs__link");
-  
+
   // Отримуємо всі елементи контенту, які відповідають вкладкам
   const contents = document.querySelectorAll(".tabs__content-text");
 
@@ -44,8 +58,8 @@ import '../scss/style.scss'
     // Додаємо подію на клік по вкладці
     tab.addEventListener("click", () => {
       // Видаляємо клас "active" у всіх вкладок та всіх елементів контенту
-      tabs.forEach(n => n.classList.remove("active"));
-      contents.forEach(m => m.classList.remove("active"));
+      tabs.forEach((n) => n.classList.remove("active"));
+      contents.forEach((m) => m.classList.remove("active"));
 
       // Додаємо клас "active" до вибраної вкладки (та яка була натиснута)
       tab.classList.add("active");
